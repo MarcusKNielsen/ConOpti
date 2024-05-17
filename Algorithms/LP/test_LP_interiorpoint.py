249f80ea23e39df650cd654101af62213c953c9d
Marianadesamadsen <s201205@student.dtu.dk>
2024-05-17 17:54:01 +0200

    LP

--- Algorithms/LP/test_LP_interiorpoint.py
+++ Algorithms/LP/test_LP_interiorpoint.py
@@ -8,8 +8,8 @@ from InteriorPoint_naive_implementation.InteriorPointLP import plotLP_2
 from InteriorPoint_naive_implementation.LP_Examples import LP_example
 
 n = 2 
-m = 1 
-np.random.seed(1) 
+m = 2 
+np.random.seed(250) 
 A = np.random.normal(0,1,(m,n)) 
 x = np.zeros(n) 
 x[:m] = abs(np.random.uniform(0,1,m)) 
@@ -24,8 +24,8 @@ b = A@x
 
 x1 = np.array([20,40])
 
-example = 4
-g,A,b,x,xlimits = LP_example(example,PrintFormat=True)
+#example = 4
+#g,A,b,x,xlimits = LP_example(example,PrintFormat=True)
 
 m,n = A.shape
 lamb1 = np.ones(n)
@@ -34,5 +34,5 @@ mu1 = np.zeros(m)
 results = InteriorPointLP(A,g,b,x1,mu1,lamb1,MaxIter=1000,tol=1e-6)
 print(results['xmin'], results['iterations'], results['Converged'])
 
-plotLP_2(g,A,b,X = results['X_results'],title=f"InteriorPointLP: Example",xlimits=[-50,50,-50,50])
+plotLP_2(g,A,b,X = results['X_results'],title=f"InteriorPointLP: Example",xlimits=[-1,5,-1,5])
 
