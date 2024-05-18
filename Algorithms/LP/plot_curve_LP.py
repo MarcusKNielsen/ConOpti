dd3910a43fb37219df9d503ff8ea56498077b5da
Marianadesamadsen <s201205@student.dtu.dk>
2024-05-18 10:14:25 +0200

    solve all func

--- Algorithms/LP/plot_curve_LP.py
+++ Algorithms/LP/plot_curve_LP.py
@@ -6,16 +6,16 @@ def plot_demand_supply_curve(U,C,xstar,idx):
     
     demand_sol = xstar[:len(U)]
     supply_sol = xstar[len(U):len(U)+len(C)]
-
+ 
     # Prepare data for plotting
-    sorted_demand_indices = np.argsort(U)  # Sort demand by price in ascending order
-    sorted_supply_indices = np.argsort(-C)  # Sort supply by price in descending order
+    sorted_demand_indices = np.argsort(U)[::-1]  
+    sorted_supply_indices = np.argsort(C) 
 
     cumulative_demand = np.cumsum(demand_sol[sorted_demand_indices])
     cumulative_supply = np.cumsum(supply_sol[sorted_supply_indices])
 
-    # Plotting
-    plt.figure(idx)
+    # Plotting 
+    plt.figure(idx) 
     plt.step(cumulative_demand, U[sorted_demand_indices],where="post", label="Cumulative Demand")
     plt.step(cumulative_supply, C[sorted_supply_indices], where="post", label="Cumulative Supply")
     plt.xlabel("Energy Quantity (MWh)")
